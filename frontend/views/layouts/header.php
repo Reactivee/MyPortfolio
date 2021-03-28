9bd1f2f5e223c85381e7d7fe7fd9501362a09103
iReact <idadajon@gmail.com>
2021-03-28 14:38:49 +0500

    lang

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -1,15 +1,32 @@
   <!-- navbar-->
   <header class="header">
       <nav class="navbar navbar-expand-lg fixed-top">
-        <div class="container"><a href="http://front.port/" class="navbar-brand scrollTo">IT Worker</a>
-          <button type="button" data-toggle="collapse" data-target="#navbarcollapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><span class="fa fa-bars"></span></button>
-          <div id="navbarcollapse" class="collapse navbar-collapse">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item"><a href="#references" class="nav-link link-scroll">My work</a></li>
-              <li class="nav-item"><a href="#customers" class="nav-link link-scroll">Clients</a></li>
-              <li class="nav-item"><a href="#contact" class="nav-link link-scroll">Contact</a></li>
-            </ul>
+          <div class="container"><a href="http://front.port/" class="navbar-brand scrollTo">IT Worker</a>
+              <button type="button" data-toggle="collapse" data-target="#navbarcollapse"
+                      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                      class="navbar-toggler navbar-toggler-right"><span class="fa fa-bars"></span></button>
+              <div id="navbarcollapse" class="collapse navbar-collapse">
+                  <ul class="navbar-nav ml-auto">
+                      <li class="nav-item"><a href="#references" class="nav-link link-scroll">My work</a></li>
+                      <li class="nav-item"><a href="#customers" class="nav-link link-scroll">Clients</a></li>
+                      <li class="nav-item"><a href="#contact" class="nav-link link-scroll">Contact</a></li>
+                  </ul>
+              </div>
+
+              <div class="list-unstyled">
+                  <li class="dropdown list-unstyled ">
+                      <a class="dropdown-toggle" href="/uz" data-toggle="dropdown" role="button" aria-expanded="false">
+                          Oʻz
+<!--                          <span class="caret"></span>-->
+                      </a>
+                      <ul class="dropdown-menu list-unstyled border-0">
+                          <li><a href="/ru">Ру</a></li>
+                          <li><a href="/en">En</a></li>
+                      </ul>
+                  </li>
+              </div>
+
+
           </div>
-        </div>
       </nav>
-    </header>
+  </header>
